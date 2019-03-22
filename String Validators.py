564db625e02ee9f5a236756d93a020244b9c1704
Le Hoang Nhan <46989247+le-hoang-nhan@users.noreply.github.com>
2019-03-22 23:14:44 +0100

    Update String Validators.py

--- String Validators.py
+++ String Validators.py
@@ -31,9 +31,11 @@ True
 
 
 
-S = raw_input()
-print any([char.isalnum() for char in S])
-print any([char.isalpha() for char in S])
-print any([char.isdigit() for char in S])
-print any([char.islower() for char in S])
-print any([char.isupper() for char in S])
+if __name__ == '__main__':
+    S = input()
+
+print (any ([char.isalnum() for char in S]))
+print (any ([char.isalpha() for char in S]))
+print (any ([char.isdigit() for char in S]))
+print (any ([char.islower() for char in S]))
+print (any ([char.isupper() for char in S]))
